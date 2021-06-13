084d3fd4876ffa8fabda5aaaae886f3c2bc7d163
emeldi <78385236+emeldi@users.noreply.github.com>
2021-06-13 12:36:27 -0700

    added AWS S3 service package

---
+++ Assets/Scripts/AWSManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+
+public class AWSManager : MonoBehaviour
+{
+    void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+}
