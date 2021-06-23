bcc7fdece92c6fa2ed04d1fda89395b87535ddf2
emeldi <78385236+emeldi@users.noreply.github.com>
2021-06-23 11:20:28 -0700

    completed photo capture functionality

--- Assets/Scripts/Panels/LocationPanel.cs
+++ Assets/Scripts/Panels/LocationPanel.cs
@@ -9,24 +9,55 @@ public class LocationPanel : MonoBehaviour, IPanel
     public RawImage mapImage;
     public InputField mapNotesInput;
 
-    public string aPIKey;
+    public string keyFrag0;
+    public string keyFrag1;
+    public string keyFrag2;
     public float latCoord, longCoord;
     public int zoom;
     public int imgSize;
     public string uRL = "https://maps.googleapis.com/maps/api/staticmap?";
 
-    private void OnEnable()
+    private IEnumerator Start()
     {
-        uRL = uRL + "center=" + latCoord + "," + longCoord + "&zoom=" + zoom + "&size=" + imgSize + "x" + imgSize + "&key=" + aPIKey;
+        //fetch geo data
+        if (Input.location.isEnabledByUser)
+        {
+            Input.location.Start();
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1.0f);
+                maxWait--;
+            }
 
-        //download static map
+            if (maxWait < 1)
+            {
+                Debug.Log("Timed Out");
+                yield break;
+            }
+
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                Debug.Log("Unable to determine device location");
+            }
+            else
+            {
+                latCoord = Input.location.lastData.latitude;
+                longCoord = Input.location.lastData.longitude;
+            }
+
+            Input.location.Stop();
+        }
+        else
+        {
+            Debug.Log("Location Services are not enabled");
+        }
         StartCoroutine(GetLocationRoutine());
-        //apply map to rawImage
-        //
     }
 
     IEnumerator GetLocationRoutine()
     {
+        uRL = uRL + "center=" + latCoord + "," + longCoord + "&zoom=" + zoom + "&size=" + imgSize + "x" + imgSize + "&key=" + keyFrag2 + keyFrag1 + keyFrag0;
         using (UnityWebRequest map = UnityWebRequestTexture.GetTexture(uRL))
         {
             yield return map.SendWebRequest();
@@ -42,7 +73,10 @@ public class LocationPanel : MonoBehaviour, IPanel
 
     public void ProcessInfo()
     {
-
+        if (!string.IsNullOrEmpty(mapNotesInput.text))
+        {
+            UIManager.Instance.activeCase.locationNotes = mapNotesInput.text;
+        }
     }
 
 }
--- Assets/Scripts/Panels/PhotoPanel.cs
+++ Assets/Scripts/Panels/PhotoPanel.cs
@@ -11,5 +11,34 @@ public class PhotoPanel : MonoBehaviour, IPanel
     {
 
     }
+    public void TakePictureButton()
+    {
+        TakePicture(400);
+    }
+    private void TakePicture(int maxSize)
+    {
+        NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                // Create a Texture2D from the captured image
+                Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
 
+                photoTaken.texture = texture;
+                photoTaken.gameObject.SetActive(true);
+
+                // If a procedural texture is not destroyed manually, 
+                // it will only be freed after a scene change
+                Destroy(texture, 5f);
+            }
+        }, maxSize);
+
+        Debug.Log("Permission result: " + permission);
+    }
 }
